400f7736837f2eafb7c7ab31aaffeeff7685ee1e
kaelsilva <kael.leak1998@gmail.com>
2019-11-25 13:04:43 -0300

    Adicionando arquivos para início da criação de um CRUD com NodeJS.

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//Iniciando o App
 const app = express();
+app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send('Hello world!');
-});
+//Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true }
+);
+
+requireDir('./src/models');
+
+//Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
